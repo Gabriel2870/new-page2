134798bef6997a534a78a1b8d0bbf7e67ecb2f87
Gabriel2870 <owinogabriel385@gmail.com>
2024-10-02 11:28:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example of basic form submission handling
+document.querySelector('form').addEventListener('submit',function(e) {
+	e.preventDefault();
+	alert('Thank you for your message!');
+});	
